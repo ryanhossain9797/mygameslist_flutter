6b0df90a5fc4ee2f8c9a86a71003e9c01d65c2a2
ryanhossain9797 <ryanhossain9797@gmail.com>
2020-02-22 16:07:50 +0600

    Implement sliver app bar in details screen

--- lib/views/details_screen.dart
+++ lib/views/details_screen.dart
@@ -21,139 +21,263 @@ class _DetailsScreenState extends State<DetailsScreen> {
     );
     return Scaffold(
       drawer: Drawer(),
-      appBar: AppBar(
-        title: Text(
-          "Details",
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Hero(
-              tag: widget.id,
-              child: Image(
-                image: widget.tempImage,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 180,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Hero(
+                  tag: widget.id,
+                  child: Image(
+                    fit: BoxFit.cover,
+                    image: widget.tempImage,
+                  ),
+                ),
               ),
             ),
-            BlocBuilder<DetailsBloc, DetailsLoadState>(
-              builder: (context, state) {
-                if (state is DetailsLoadingState) {
-                  return Expanded(
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                } else if (state is DetailsFailedState) {
-                  return Expanded(
-                    child: Center(
-                      child: Text("error"),
-                    ),
-                  );
-                } else {
-                  WikiModel article = (state as DetailsLoadedState).article;
-                  List<ReviewModel> reviews =
-                      (state as DetailsLoadedState).reviews;
-                  return Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10.0,
+          ];
+        },
+        body: BlocBuilder<DetailsBloc, DetailsLoadState>(
+          builder: (context, state) {
+            if (state is DetailsLoadingState) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is DetailsFailedState) {
+              return Center(
+                child: Text("error"),
+              );
+            } else {
+              WikiModel article = (state as DetailsLoadedState).article;
+              List<ReviewModel> reviews = (state as DetailsLoadedState).reviews;
+              return SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      child: Text(
+                        article.title,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Poppins',
+                          color: Colors.lightGreenAccent,
+                        ),
                       ),
-                      child: SingleChildScrollView(
-                        physics: BouncingScrollPhysics(),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                article.title,
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontFamily: 'Poppins',
-                                  color: Colors.lightGreenAccent,
-                                ),
-                              ),
-                              padding: EdgeInsets.all(20),
-                            ),
-                            Container(
-                              padding: EdgeInsets.symmetric(horizontal: 5),
-                              child: Text(
-                                article.content,
-                                style: TextStyle(
-                                  fontFamily: 'Poppins',
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              "Reviews",
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontFamily: "Poppins",
-                                color: Colors.lightGreenAccent,
-                              ),
-                            ),
-                            ListView.builder(
-                              shrinkWrap: true,
-                              primary: false,
-                              itemCount: reviews.length,
-                              itemBuilder: (BuildContext context, int pos) {
-                                return Container(
-                                  margin: EdgeInsets.symmetric(vertical: 5),
-                                  padding: EdgeInsets.all(10),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.grey[800],
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Text(
-                                        reviews[pos].username,
-                                        style: TextStyle(
-                                          color: Colors.lightGreenAccent,
-                                          fontSize: 20,
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 10,
-                                      ),
-                                      Text(reviews[pos].review),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                            ReviewWidget(
-                              onReviewed: (username, review) {
-                                BlocProvider.of<DetailsBloc>(context).add(
-                                  ReviewDetailsEvent(
-                                    review: ReviewModel(
-                                      id: article.id,
-                                      username: username,
-                                      review: review,
-                                    ),
-                                  ),
-                                );
-                              },
-                            )
-                          ],
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        article.content,
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
                         ),
                       ),
                     ),
-                  );
-                }
-              },
-            ),
-          ],
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Reviews",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: "Poppins",
+                        color: Colors.lightGreenAccent,
+                      ),
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      primary: false,
+                      itemCount: reviews.length,
+                      itemBuilder: (BuildContext context, int pos) {
+                        return Container(
+                          margin:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey[800],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                reviews[pos].username,
+                                style: TextStyle(
+                                  color: Colors.lightGreenAccent,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(reviews[pos].review),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: ReviewWidget(
+                        onReviewed: (username, review) {
+                          BlocProvider.of<DetailsBloc>(context).add(
+                            ReviewDetailsEvent(
+                              review: ReviewModel(
+                                id: article.id,
+                                username: username,
+                                review: review,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }
+          },
         ),
       ),
+    );
+  }
+}
+
+class BodyBloc extends StatelessWidget {
+  const BodyBloc({
+    Key key,
+    @required this.widget,
+  }) : super(key: key);
+
+  final DetailsScreen widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<DetailsBloc, DetailsLoadState>(
+      builder: (context, state) {
+        if (state is DetailsLoadingState) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is DetailsFailedState) {
+          return Center(
+            child: Text("error"),
+          );
+        } else {
+          return CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 180,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image(
+                    fit: BoxFit.cover,
+                    image: widget.tempImage,
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, position) {
+                  WikiModel article = (state as DetailsLoadedState).article;
+                  List<ReviewModel> reviews =
+                      (state as DetailsLoadedState).reviews;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              article.title,
+                              style: TextStyle(
+                                fontSize: 30,
+                                fontFamily: 'Poppins',
+                                color: Colors.lightGreenAccent,
+                              ),
+                            ),
+                            padding: EdgeInsets.all(20),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 5),
+                            child: Text(
+                              article.content,
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Reviews",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontFamily: "Poppins",
+                              color: Colors.lightGreenAccent,
+                            ),
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            primary: false,
+                            itemCount: reviews.length,
+                            itemBuilder: (BuildContext context, int pos) {
+                              return Container(
+                                margin: EdgeInsets.symmetric(vertical: 5),
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.grey[800],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      reviews[pos].username,
+                                      style: TextStyle(
+                                        color: Colors.lightGreenAccent,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(reviews[pos].review),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                          ReviewWidget(
+                            onReviewed: (username, review) {
+                              BlocProvider.of<DetailsBloc>(context).add(
+                                ReviewDetailsEvent(
+                                  review: ReviewModel(
+                                    id: article.id,
+                                    username: username,
+                                    review: review,
+                                  ),
+                                ),
+                              );
+                            },
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }, childCount: 1),
+              ),
+            ],
+          );
+        }
+      },
     );
   }
 }
